cfaa11dcc0ce06974dce49242a05c4df4c247400
Krishna Kumar <krishnakumar@Krishnas-MacBook-Air.local>
2020-01-06 12:34:01 +0530

    travis added

---
+++ build/precache-manifest.e6892116354df80ba0d04c0659f5b193.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f237f461c69ea1dc2ca876cb7a50371",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b97eb5e2fe6e82005d9c",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "816bcb79b2593f548958",
+    "url": "/static/js/2.3228c993.chunk.js"
+  },
+  {
+    "revision": "b97eb5e2fe6e82005d9c",
+    "url": "/static/js/main.ad984ccb.chunk.js"
+  },
+  {
+    "revision": "bb7e7463357611dc8d83",
+    "url": "/static/js/runtime-main.fca44c8b.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
